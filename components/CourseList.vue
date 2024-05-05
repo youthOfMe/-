3b4d9e29bdffc9b60cb9eaa718da0dce038d4528
youthOfMe <2040690180@qq.com>
2024-05-05 22:20:40 +0800

    feat: 价格组件封装

--- components/CourseList.vue
+++ components/CourseList.vue
@@ -5,8 +5,9 @@
     </template>
     <div class="pt-4">
       <span class="font-fold w-full truncate font-semibold">{{ item?.title }}</span>
-      <div class="mt-2 flex">
-        价格
+      <div class="mt-2 flex items-end">
+        <Price :value="item?.price" />
+        <Price :value="item?.t_price" through class="ml-2" />
       </div>
     </div>
   </n-card>
